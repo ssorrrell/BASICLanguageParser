d2645d7af79a7ea105786c55a4d5d85e9d85b5cb
ssorrrell <ssorrrell@hotmail.com>
2022-01-02 21:36:49 -0600

    Update UTs

--- ECB2_Lib.Parser.UT/jvmBasicParserTest.cs
+++ ECB2_Lib.Parser.UT/jvmBasicParserTest.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
 using ECB2_Lib.Parser;
+using System.IO;
 
 namespace ECB2_Lib.Parser.UT
 {
@@ -34,6 +35,16 @@ namespace ECB2_Lib.Parser.UT
             //Assert.Pass();
 
             string txt = "ABS(-5)";
+            int result = RunABS(txt);
+            Assert.AreEqual(0, result, "Wrong Error Count ABS Case: '{0}'", txt);
+
+            txt = "ABS\"G\")";
+            result = RunABS(txt);
+            Assert.AreEqual(1, result, "Wrong Error Count ABS Case: '{0}'", txt);
+        }
+
+        private int RunABS(string txt)
+        {
             AntlrInputStream inputStream = new AntlrInputStream(txt);
             jvmBasicLexer lexer = new jvmBasicLexer(inputStream);
             lexer.AddErrorListener(new LexErrorListener());
@@ -43,21 +54,40 @@ namespace ECB2_Lib.Parser.UT
             jvmBasicParser.AbsfuncContext absDefinitionContext = parser.absfunc();
             jvmBasicBaseVisitor<object> visitor = new jvmBasicBaseVisitor<object>();
             visitor.Visit(absDefinitionContext);
-            Assert.True(parser.NumberOfSyntaxErrors == 0);
             parser.RemoveErrorListeners();
-
-            txt = "ABS\"G\")";
-            inputStream = new AntlrInputStream(txt);
-            lexer = new jvmBasicLexer(inputStream);
-            lexer.AddErrorListener(new LexErrorListener());
-            commonTokenStream = new CommonTokenStream(lexer);
-            parser = new jvmBasicParser(commonTokenStream);
-            parser.AddErrorListener(new ErrorListener());
-            absDefinitionContext = parser.absfunc();
-            visitor = new jvmBasicBaseVisitor<object>();
-            visitor.Visit(absDefinitionContext);
-            Assert.True(parser.NumberOfSyntaxErrors == 1);
-            parser.RemoveErrorListeners();
+            return parser.NumberOfSyntaxErrors;
         }
-    }
+
+        [Test]
+        public void ProgTests()
+        {
+            string filename = "bottles_of_beer.bas";
+            int result = RunProg(filename);
+            Assert.AreEqual(0, result, "Wrong Error Count ProgTest Case: {0}", filename);
+
+            filename = "hello_world.bas";
+            result = RunProg(filename);
+            Assert.AreEqual(0, result, "Wrong Error Count ProgTest Case: {0}", filename);
+        }
+
+        private int RunProg(string filename)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, @"..\..\..\bas\", filename);
+            AntlrFileStream stream = new AntlrFileStream(path);
+            jvmBasicLexer lexer = new jvmBasicLexer(stream);
+            LexErrorListener lexErrorListener = new LexErrorListener();
+            lexer.AddErrorListener(lexErrorListener);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+            jvmBasicParser parser = new jvmBasicParser(commonTokenStream);
+            ErrorListener parseErrorListener = new ErrorListener();
+            parser.AddErrorListener(parseErrorListener);
+            jvmBasicParser.ProgContext progContext = parser.prog();
+            jvmBasicBaseVisitor<object> visitor = new jvmBasicBaseVisitor<object>();
+            visitor.Visit(progContext);
+            parser.RemoveErrorListeners();
+            return lexErrorListener.Errors.Count + parseErrorListener.Errors.Count;
+        }
+
+
+     }
 }
